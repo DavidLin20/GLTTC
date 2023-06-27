0a671e8ad16aebb9339863236ae3b32a57ae8805
macnoblebk <macnoblebk@gmail.com>
2023-06-27 16:21:50 -0700

    Adds input view

---
+++ GLTTC/Components/InputView.swift
@@ -0,0 +1,20 @@
+//
+//  InputView.swift
+//  GLTTC
+//
+//  Created by Mac-Noble Brako-Kusi on 6/27/23.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InputView_Previews: PreviewProvider {
+    static var previews: some View {
+        InputView()
+    }
+}
