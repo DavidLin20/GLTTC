62589bca5ef624bcf0f33537f7648ba44d8f50b3
DavidLin20 <tiweilin20@gmail.com>
2023-06-27 14:43:27 -0700

    Initial Commit

---
+++ GLTTC/GLTTCApp.swift
@@ -0,0 +1,17 @@
+//
+//  GLTTCApp.swift
+//  GLTTC
+//
+//  Created by Ti Wei Lin on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct GLTTCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
