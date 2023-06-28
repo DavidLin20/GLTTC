e7f0c2b093ee74bf6ecef70217650af7ecb2460c
macnoblebk <macnoblebk@gmail.com>
2023-06-27 17:04:48 -0700

    Create Login view

--- GLTTC/Core/Authentication/LoginView.swift
+++ GLTTC/Core/Authentication/LoginView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
         NavigationStack{
             VStack{
@@ -16,7 +19,45 @@ struct LoginView: View {
                     .scaledToFit()
                     .frame(width:500, height:120)
                     .padding(.vertical,32)
+                
+                VStack(spacing: 24){
+                    InputView(text: $email, title: "Email Address", placeholder: "name@example.com")
+                        .autocapitalization(.none)
+                    
+                    InputView(text: $password, title: "Password", placeholder: "Enter your password", isSecureField: true)
+                    
+                }
+                .padding(.horizontal)
+                .padding(.top, 12)
+                
+                Button{
+                    print("Log user in...")
+                } label: {
+                    HStack{
+                        Text("Sign In")
+                            .fontWeight(.semibold)
+                        Image(systemName: "arrow.right")
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+                }
+                
+                .background(Color(.systemBlue))
+                .cornerRadius(10)
+                .padding(.top, 24)
+                
                 Spacer()
+                
+                NavigationLink {
+                    
+                } label: {
+                    HStack(spacing: 3){
+                        Text("Don't have an account?")
+                        Text("Sign Up")
+                            .fontWeight(.bold)
+                    }
+                    .font(.system(size: 14))
+                }
                 
             }
         }
