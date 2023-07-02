bb9097bdf179ad3a9f24a65249c87c01841342e5
DavidLin20 <tiweilin20@gmail.com>
2023-07-01 17:13:43 -0700

    Implemented check in and check out alert

--- GLTTC/Core/League/LeagueView.swift
+++ GLTTC/Core/League/LeagueView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct LeagueView: View {
+    @State private var accessCode = ""
     @State private var showAlert = false
     @State var selectedTab: TopTabs = .players
     @State var isCheckedIn: Bool = false
@@ -33,54 +34,75 @@ struct LeagueView: View {
         Spacer()
         
         VStack {
+            // Check in Button
+            if (!isCheckedIn) {
                 Button(action: {
-//                    isCheckedIn.toggle()
+                    //                    isCheckedIn.toggle()
                     showAlert = true
                 }) {
                     HStack {
-                        Text("Check \(isCheckedIn ? "Out" : "In")")
+                        Text("Check In")
                             .fontWeight(.semibold)
                             .foregroundColor(.white)
-                        Image(systemName: isCheckedIn ? "figure.walk.departure" : "figure.walk.arrival")
+                        Image(systemName: "figure.walk.arrival")
                             .foregroundColor(.white)
                     }
                     .frame(width: UIScreen.main.bounds.width - 85, height: 40)
-                    .background(isCheckedIn ? Color.red : Color.blue)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    .padding(.top, 24)
+                }
+                .alert("Check in", isPresented: $showAlert, actions: {
+                    TextField("Access Code", text: $accessCode)
+                    
+                    Button(action: {}) {
+                        Text("Cancel")
+                            .foregroundColor(.red)
+                    }
+                    
+                    Button("Confirm", action: {
+                        isCheckedIn.toggle()
+                        Task {
+                            await viewModel.checkUserIn(isCheckedIn: isCheckedIn)
+                        }
+                    })
+                }, message: {
+                    Text("Enter today's access code")
+                })
+                
+            } else {
+                // Check out button
+                
+                Button(action: {
+                    //                    isCheckedIn.toggle()
+                    showAlert = true
+                }) {
+                    HStack {
+                        Text("Check Out")
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                        Image(systemName: "figure.walk.departure")
+                            .foregroundColor(.white)
+                    }
+                    .frame(width: UIScreen.main.bounds.width - 85, height: 40)
+                    .background(Color.red)
                     .cornerRadius(10)
                     .padding(.top, 24)
                 }
                 .alert(isPresented: $showAlert) {
-                    Alert(title: Text("Check \(isCheckedIn ? "Out" : "In")"),
+                    Alert(title: Text("Check Out"),
                           message:
-                            Text("Are you sure you want to \(isCheckedIn ? "check out" : "check in")?"),
+                            Text("Are you sure you want to check out?"),
                           primaryButton: .default(Text("Confirm"), action: {
-                              isCheckedIn.toggle() // Toggle isCheckedIn here
-                              Task {
-                                  await viewModel.checkUserIn(isCheckedIn: isCheckedIn)
-                              }
-                          }),
+                        isCheckedIn.toggle() // Toggle isCheckedIn here
+                        Task {
+                            await viewModel.checkUserIn(isCheckedIn: isCheckedIn)
+                        }
+                    }),
                           secondaryButton: .cancel(Text("Cancel")))
                 }
             }
-
-//        Button {
-//            isCheckedIn.toggle()
-//            Task {
-//                await viewModel.checkUserIn(isCheckedIn: isCheckedIn)
-//            }
-//        } label: {
-//            HStack {
-//                Text("Check \(isCheckedIn ? "Out" : "In")")
-//                    .fontWeight(.semibold)
-//                    .foregroundColor(.white)
-//                Image(systemName: isCheckedIn ? "figure.walk.departure" : "figure.walk.arrival")
-//                    .foregroundColor(.white) // Set the same color as the text
-//            }
-//            .frame(width: UIScreen.main.bounds.width - 85, height: 40)
-//            .background(isCheckedIn ? Color.red : Color.blue)
-//            .cornerRadius(10)
-//            .padding(.top, 24)
-//        }
+        }
     }
 }
 
