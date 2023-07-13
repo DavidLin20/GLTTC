c947963892618ce0a01c945f88e8a4ae7e34f289
macnoblebk <macnoblebk@gmail.com>
2023-07-12 17:51:38 -0700

    Creates OnDeck class and OnDeckViewModel

---
+++ GLTTC/Core/Model/OnDeck.swift
@@ -0,0 +1,15 @@
+//
+//  OnDeck.swift
+//  GLTTC
+//
+//  Created by Mac-Noble Brako-Kusi on 7/12/23.
+//
+
+import Foundation
+
+struct OnDeck:  Codable {
+    var playerOne : User
+    var palyerTwo: User
+    
+    
+}
---
+++ GLTTC/Core/OnDeck/OnDeckViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  OnDeckViewModel.swift
+//  GLTTC
+//
+//  Created by Mac-Noble Brako-Kusi on 7/12/23.
+//
+
+import Foundation
+
+
